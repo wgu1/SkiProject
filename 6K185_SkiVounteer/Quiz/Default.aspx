f4392245e7513566dcfc2776db9313e7d0bbf275
Yile Wu <yile-wu@uiowa.edu>
2013-10-13 21:10:36 -0500

    add quiz function

---
+++ 6K185_SkiVounteer/Quiz/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/Site.master" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="Quiz_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <asp:Panel ID="Panel1" runat="server">
+         
+    </asp:Panel>
+    <asp:Panel ID="Panel2" runat="server">
+         <div>
+        <p id="P1">This is question 1</p>
+        <asp:RadioButton ID="RadioButton5" Text ="This is Anser A" runat="server" GroupName ="haha"/> 
+        <asp:RadioButton ID="RadioButton6" Text ="This is Anser A" runat="server" GroupName ="haha"/> 
+        <asp:RadioButton ID="RadioButton7" Text ="This is Anser A" runat="server" GroupName ="haha"/> 
+        <asp:RadioButton ID="RadioButton8" Text ="This is Anser A" runat="server" GroupName ="haha"/> 
+        </div>
+    </asp:Panel>
+</asp:Content>
+
---
+++ 6K185_SkiVounteer/Quiz/Default.aspx.vb
@@ -0,0 +1,35 @@
+﻿
+Partial Class Quiz_Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If (IsPostBack = False) Then
+            Dim ct As Integer = 1 ' this is the question counter
+            Dim i As Integer
+            For i = ct To 10
+                'this is the new radiobutton
+                Dim arrRButton(3) As RadioButton
+                Dim Pan As New Panel
+                'add on panel
+                Pan.ID = "ha" + Convert.ToString(ct)
+
+                Me.Controls.Add(Pan)
+                With Panel1
+                    arrRButton(0) = New RadioButton
+                    arrRButton(1) = New RadioButton
+                    arrRButton(2) = New RadioButton
+                    arrRButton(3) = New RadioButton
+                    .Controls.Add(arrRButton(0))
+                    .Controls.Add(New LiteralControl(("<br />"))) 'this is a line breaker between controls
+                    .Controls.Add(arrRButton(1))
+                    .Controls.Add(New LiteralControl(("<br />"))) 'this is a line breaker between controls
+                    .Controls.Add(arrRButton(2))
+                    .Controls.Add(New LiteralControl(("<br />"))) 'this is a line breaker between controls
+                    .Controls.Add(arrRButton(3))
+                    .Controls.Add(New LiteralControl(("<br />"))) 'this is a line breaker between controls
+                End With
+                'add one more panel
+            Next
+        End If
+    End Sub
+End Class
