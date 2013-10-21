89b111a899d57b8a50e5bf9a1bf74db14773c2af
anwillia <andrew-williams-1@uiowa.edu>
2013-10-20 19:55:12 -0500

    modified committee information page

--- 6K185_SkiVounteer/CommitteeInformation.aspx
+++ 6K185_SkiVounteer/CommitteeInformation.aspx
@@ -3,13 +3,16 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
-    <asp:DropDownList ID="CommitteeDropDownList" runat="server" DataSourceID="SqlDataSource1" DataValueField="CommitteeName"></asp:DropDownList>
-
-    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:fk185_ClassConnectionString %>" SelectCommand="SELECT [CommitteeID], [CommitteeName] FROM [Ski_Committee]"></asp:SqlDataSource>
-
+       <asp:DropDownList ID="ddlCommittee" runat="server" AutoPostBack="True" 
+                             DataTextField="CommitteeName" DataValueField="CommitteeID" DataSourceID="SqlDataSource1">
+                        </asp:DropDownList>
+    
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:fk185_ClassConnectionString %>" SelectCommand="SELECT * FROM [Ski_Committee]"></asp:SqlDataSource>
+    
     <br />
-    <iframe width="420" height="315" src="//www.youtube.com/embed/rjOhRqB-KHk" frameborder="0" allowfullscreen></iframe>
-
-    <p>Here is some code on how to embed a flash movie file: <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="http://stackoverflow.com/questions/668846/how-to-embed-a-flash-swf-file-into-asp-net">StackFlow for embeddding flash videos</asp:HyperLink></p>
+    <iframe src=<%=DocumentName%> width="810px" height="500px" scrolling="yes" frameborder="0"></iframe>
+    <br />
+    <iframe width="420" height="315" src=<%=VideoURL%> frameborder="0" allowfullscreen></iframe>
+ 
 </asp:Content>
 
--- 6K185_SkiVounteer/CommitteeInformation.aspx.vb
+++ 6K185_SkiVounteer/CommitteeInformation.aspx.vb
@@ -1,5 +1,112 @@
-﻿
+﻿Imports System.Data.SqlClient
+
 Partial Class Video
     Inherits System.Web.UI.Page
+    Private _VideoUrl As String
+    Private _DocumentName As String
+    Protected Property VideoURL As String
+        Get
+            Return _VideoURL
+        End Get
+        Set(value As String)
+        End Set
+    End Property
+    Protected Property DocumentName As String
+        Get
+            Return _DocumentName
+        End Get
+        Set(value As String)
+        End Set
+    End Property
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs)
+        Dim CommitteeID As String
+        Dim sql As String
+        Dim dr As SqlDataReader
+        Dim strConnectionString As String
+        Dim objCommand As SqlCommand
 
+        strConnectionString = ConfigurationManager.ConnectionStrings("fk185_ClassConnectionString").ConnectionString
+        Dim sqlConn As New SqlConnection(strConnectionString)
+        sql = "Select Top CommitteeID From Ski_Committee"
+        objCommand = New SqlCommand(sql, sqlConn)
+        sqlConn.Open()
+        dr = objCommand.ExecuteReader()
+        While dr.Read
+            CommitteeID = dr("CommitteeID")
+        End While
+        dr.Close()
+
+        sql = "Select VideoURL From Ski_Video Where CommitteeID = @CommitteeID"
+        objCommand = New SqlCommand(sql, sqlConn)
+        objCommand.Parameters.AddWithValue("@CommitteeID", CommitteeID)
+        dr = objCommand.ExecuteReader()
+        While dr.Read
+            _VideoUrl = dr("VideoURL")
+        End While
+        dr.Close()
+
+        sql = "Select FilePath, DocumentName From Ski_Committee Inner Join Ski_Document On Ski_Committee.DocumentID = Ski_Document.DocumentID Where CommitteeID = @CommitteeID"
+        objCommand = New SqlCommand(sql, sqlConn)
+        objCommand.Parameters.AddWithValue("@CommitteeID", CommitteeID)
+        dr = objCommand.ExecuteReader()
+        While dr.Read
+            _DocumentName = dr("FilePath") & dr("DocumentName")
+        End While
+        dr.Close()
+        sqlConn.Close()
+
+    End Sub
+    Protected Sub ddlCommittee_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ddlCommittee.SelectedIndexChanged
+        Dim CommitteeID As String
+        Dim sql As String
+        Dim dr As SqlDataReader
+        Dim strConnectionString As String
+        Dim objCommand As SqlCommand
+
+        strConnectionString = ConfigurationManager.ConnectionStrings("fk185_ClassConnectionString").ConnectionString
+        Dim sqlConn As New SqlConnection(strConnectionString)
+        CommitteeID = ddlCommittee.SelectedValue
+
+        sql = "Select VideoURL From Ski_Video Where CommitteeID = @CommitteeID"
+        objCommand = New SqlCommand(sql, sqlConn)
+        objCommand.Parameters.AddWithValue("@CommitteeID", CommitteeID)
+        sqlConn.Open()
+        dr = objCommand.ExecuteReader()
+        While dr.Read
+            _VideoUrl = dr("VideoURL")
+        End While
+        dr.Close()
+
+        sql = "Select FilePath, DocumentName From Ski_Committee Inner Join Ski_Document On Ski_Committee.DocumentID = Ski_Document.DocumentID Where CommitteeID = @CommitteeID"
+        objCommand = New SqlCommand(sql, sqlConn)
+        objCommand.Parameters.AddWithValue("@CommitteeID", CommitteeID)
+        dr = objCommand.ExecuteReader()
+        While dr.Read
+            _DocumentName = dr("FilePath") & dr("DocumentName")
+        End While
+        dr.Close()
+        sqlConn.Close()
+
+    End Sub
+    Protected Sub BindDDLCommittee()
+        Dim strSql As String
+        Dim objCommand As SqlCommand
+
+        Dim strConnectionString As String
+        strConnectionString = ConfigurationManager.ConnectionStrings("fk185_ClassConnectionString").ConnectionString
+        Dim sqlConn As New SqlConnection(strConnectionString)
+
+        strSql = "Select * from Ski_Committee"
+        sqlConn.Open()
+
+        objCommand = New SqlCommand(strSql, sqlConn)
+
+        ddlCommittee.DataSource = objCommand.ExecuteReader()
+        ddlCommittee.DataBind()
+
+        sqlConn.Close()
+        ddlCommittee.Items.Insert(0, "Select a Committee")
+        ddlCommittee.Items.Item(0).Value = -1
+
+    End Sub
 End Class
